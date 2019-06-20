4895c86db62e5c4986c0c8293ce45978b9f35a27
iture123 <1011699225@qq.com>
2019-06-20 19:43:48 +0800

    Update ExecutorFactory.java

--- helloJob/src/main/java/com/helloJob/jobExecutor/ExecutorFactory.java
+++ helloJob/src/main/java/com/helloJob/jobExecutor/ExecutorFactory.java
@@ -1,16 +1,27 @@
 package com.helloJob.jobExecutor;
 
-import com.helloJob.constant.HostConst;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import com.alibaba.fastjson.JSONObject;
 import com.helloJob.jobExecutor.impl.LocalExecutor;
 import com.helloJob.jobExecutor.impl.SshExector;
+import com.helloJob.model.job.HostInfo;
 import com.helloJob.model.job.JobBasicInfo;
+import com.helloJob.service.job.HostInfoService;
+import com.helloJob.utils.ApplicationContextUtil;
 
 public class ExecutorFactory {
+	public static final Logger log = LoggerFactory.getLogger(ExecutorFactory.class.getName());
 	public static AbstractExecutor make(JobBasicInfo job, Integer dt) {
-		int hostId = job.getHostId();
-		if(hostId == HostConst.LOCALHOST) {
+		ApplicationContext context = ApplicationContextUtil.getContext();
+		HostInfoService hostInfoService = context.getBean(HostInfoService.class);	
+		HostInfo hostInfo = hostInfoService.get(job.getHostId());
+		//log.info(JSONObject.toJSONString(hostInfo));
+		if(hostInfo.getProtocol().equals("本地执行")) {
 			return new LocalExecutor(job, dt);
-		}else if(hostId == HostConst.SSH) {
+		}else if(hostInfo.getProtocol().equals("ssh")) {
 			return new SshExector(job, dt);
 		}else {
 			return null;
